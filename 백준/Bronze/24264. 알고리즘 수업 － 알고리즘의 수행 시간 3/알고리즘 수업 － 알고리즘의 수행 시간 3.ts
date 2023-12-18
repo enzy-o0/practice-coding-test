d9a01433fae485c0ef4bda0e072d944e2a50945b
enzy-o0 <83004037+enzy-o0@users.noreply.github.com>
2023-12-18 21:53:17 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.ts
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+const input = fs.readFileSync(filepath).toString().trim().split("\n");
+
+console.log(input * input);
+console.log(2);
