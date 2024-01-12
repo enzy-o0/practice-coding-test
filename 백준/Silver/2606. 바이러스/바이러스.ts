5e2580936c3c28d87993497ec1b4301354c5b689
enzy-o0 <83004037+enzy-o0@users.noreply.github.com>
2024-01-12 21:37:12 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 9376 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.ts
+++ 백준/Silver/2606. 바이러스/바이러스.ts
@@ -1,26 +1,35 @@
-let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-let node = Number(input[0]);
-let edge_num = Number(input[1]);
-let graph = [...new Array(node + 1)].map(() => []);
-let visited = [...new Array(node + 1)].fill(0);
-let ans = 0;
-// 그래프 생성
-for (let i = 0; i < edge_num; i++) {
-  let start = Number(input[i + 2].split(" ")[0]);
-  let end = Number(input[i + 2].split(" ")[1]);
-  graph[start].push(end);
-  graph[end].push(start);
+const fs = require("fs");
+const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+const input = fs.readFileSync(filepath).toString().trim().split("\n");
+
+const computerTotalCount = Number(input.shift());
+const addEdge = Number(input.shift());
+
+const vertexes = {};
+
+for (let i = 0; i < computerTotalCount + 1; i++) {
+    vertexes[i] = [];
 }
-// 1번노드 방문처리해주고 dfs 시작
-visited[1] = 1;
-function dfs(start) {
-  for (let end of graph[start]) {
-    if (!visited[end]) {
-      visited[end] = 1;
-      ans++;
-      dfs(end);
-    }
-  }
+ 
+while (input.length > 0) {
+    const [vertex1, vertex2] = input.shift().split(" ").map(Number);
+    vertexes[vertex1].push(vertex2);
+    vertexes[vertex2].push(vertex1);
+}
+
+let count = 0;
+let visited = {};
+
+function dfs(startVertex) {
+    if (!startVertex) return;
+    visited[startVertex] = true;
+    vertexes[startVertex].forEach(neighbor => {
+        if (visited[neighbor]) return;
+        count++;
+        return dfs(neighbor);
+    });
+    
 }
 dfs(1);
-console.log(ans);
+console.log(count);
